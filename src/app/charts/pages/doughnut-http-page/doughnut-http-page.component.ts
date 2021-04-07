6a0b327281e2a33056eb76520c43c5a212cee65b
joelgrod <joelgrod@gmail.com>
2021-04-07 06:30:27 +0100

    doughnut http page

--- src/app/charts/pages/doughnut-http-page/doughnut-http-page.component.ts
+++ src/app/charts/pages/doughnut-http-page/doughnut-http-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, MultiDataSet } from 'ng2-charts';
 
 @Component({
   selector: 'app-doughnut-http-page',
@@ -7,6 +8,23 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class DoughnutHttpPageComponent implements OnInit {
+
+  // Doughnut
+  public testLabels: string[] = [
+    'Download Sales', 'In-Store Sales', 'Mail-Order Sales'
+  ];
+  public testChartData: MultiDataSet = [
+    [350, 450, 100]
+  ];
+  public colors: Color[] = [
+    {
+      backgroundColor: [
+        '#8E33F2',
+        '#33A4F2',
+        '#3577FC'
+      ]
+    }
+  ]
 
   constructor() { }
 
